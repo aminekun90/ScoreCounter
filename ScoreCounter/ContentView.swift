38bb324dafe2c2c9272b3f58529afab9d3bc9852
Amine Bouzahar <mohamed.amine.bouzahar@gmail.com>
2024-02-21 12:57:50 +0100

    Initial Commit

---
+++ ScoreCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScoreCounter
+//
+//  Created by Amine Bouzahar on 21/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScoreCounter/ScoreCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreCounterApp.swift
+//  ScoreCounter
+//
+//  Created by Amine Bouzahar on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScoreCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
