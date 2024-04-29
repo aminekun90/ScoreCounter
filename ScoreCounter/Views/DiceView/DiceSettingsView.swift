8b506a2bdabc14c1cbcb5631c896bf6193b0ba37
Amine Bouzahar <mohamed.amine.bouzahar@gmail.com>
2024-04-29 17:44:58 +0200

    feat: enhance dice options (WIP)

--- ScoreCounter/Views/DiceView/DiceSettingsView.swift
+++ ScoreCounter/Views/DiceView/DiceSettingsView.swift
@@ -6,17 +6,71 @@
 //
 
 import SwiftUI
+struct SimpleValueId{
+    let id = UUID()
+    let value: String
+}
+struct DiceButtonsView: View {
+    @Binding var dicesSidesLabel:[SimpleValueId]
+    @Binding var numberOfDices:Int
+    @Binding var numberOfDicesSides:Int
+
+    var body: some View {
+        HStack {
+            ForEach(dicesSidesLabel, id: \.id) { label in
+                Button(action: {
+                    print("Button \(label.value) pressed")
+                    if label.value.range(of: "x",options:.caseInsensitive) != nil {
+                        // to do display an input to use it
+                    }
+                    
+                }) {
+                    Text(label.value)
+                        .font(.custom("Oswald-Bold", size: 20))
+                        .padding()
+                        .frame(width: 70, height: 50)
+                        .background(.blue)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                }.buttonStyle(BorderlessButtonStyle())
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
 
 struct DiceSettingsView: View {
     @Binding var isPresented: Bool
     @State var numberOfDices:Int = 1
+    @State var numberOfDicesSides:Int = 6
     @State var enableShakeToRoll: Bool = true
     @State var enableSound: Bool = true
+    @State var dicesNumberLabel = [
+        SimpleValueId(value: "1"),
+        SimpleValueId(value:"2"),
+        SimpleValueId(value:"4"),
+        SimpleValueId(value:"5"),
+        SimpleValueId(value:"X")
+    ]
+    @State var dicesSidesLabel = [
+        SimpleValueId(value:"D6"),
+        SimpleValueId(value:"D8"),
+        SimpleValueId(value:"D12"),
+        SimpleValueId(value:"D20"),
+        SimpleValueId(value:"DX")
+    ]
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Settings")) {
-                    
+                    DiceButtonsView(dicesSidesLabel: $dicesNumberLabel,numberOfDices: $numberOfDices,numberOfDicesSides: $numberOfDicesSides)
+                    HStack(alignment:.center){
+                        Text("X")
+                            .font(.custom("Oswald-Bold", size: 20))
+                            .foregroundStyle(.orange)
+                        
+                    }.frame(maxWidth: .infinity)
+                    DiceButtonsView(dicesSidesLabel: $dicesSidesLabel, numberOfDices: $numberOfDices,numberOfDicesSides: $numberOfDicesSides)
                     Toggle("Shake to roll", isOn: $enableShakeToRoll)
                     Toggle("Play sound", isOn: $enableSound)
                 }
